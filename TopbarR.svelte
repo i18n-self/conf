965ddb29fde9eb37bdfffc3239651688af4462b5
i18n <i18n.site@gmail.com>
2024-10-14 10:35:43 +0800

    🔥

---
+++ TopbarR.svelte
@@ -0,0 +1,17 @@
+<script lang="coffee">
+> @8p/full:Full
+  @8p/auth/btn/Auth.svelte
+  @8p/lang/Lang.svelte
+  @8p/qy/Search.svelte
+</script>
+
+<template lang="pug">
+Search
+aside
+  Full
+  Lang
+Auth
+</template>
+
+<style lang="stylus"></style>
+
---
+++ plugin.js
@@ -0,0 +1,4 @@
+import "@8p/auth/init.js"
+import "@8p/pay"
+import "@8p/token"
+import "@8p/github"
